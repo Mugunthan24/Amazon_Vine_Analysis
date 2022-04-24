7a8edefa5c30788f33efcc3bd119e3d949b2be01
Mugunthan24 <mrengarajah@gmail.com>
2022-04-24 15:25:39 -0400

    Adding updated README, and images.

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # Amazon_Vine_Analysis
-Using PySpark and AWS to analyze big data
+Using PySpark and AWS to analyze big data.
+
+## Overview of Analysis
+A dataset for videogame reviews written by members of the paid Amazon Vine program is being analyzed. PySpark was used to perform the ETL process to extract the dataset, transform the data, connect to an AWS RDS instance, and load the transformed data into pgAdmin. Subsequently, SQL was then used to determine if there is any bias toward favorable reviews from Vine members in the dataset.
+
+## Results
+The subsequent sections will provide information for each group (vine members vs. non-vine members).
+
+### Number of Paid Reviews
+![image_name](https://github.com/Mugunthan24/Amazon_Vine_Analysis/blob/main/Images/Number_of_Paid_Reviews.png)
+
+The total number of paid reviews is 90.
+
+### Number of Unpaid Reviews
+![image_name](https://github.com/Mugunthan24/Amazon_Vine_Analysis/blob/main/Images/Number_of_unpaid_Reviews.png)
+
+The total number of unpaid reviews is 37,831.
+
+### Number of Paid 5-Star Reviews
+![image_name](https://github.com/Mugunthan24/Amazon_Vine_Analysis/blob/main/Images/Number_of_Paid_5-Star_Reviews.png)
+
+The total number of unpaid reviews is 44.
+
+### Number of Unpaid 5-Star Reviews
+![image_name](https://github.com/Mugunthan24/Amazon_Vine_Analysis/blob/main/Images/Number_of_Unpaid_5-Star_Reviews.png)
+
+The total number of unpaid reviews is 14,704.
+
+### Percentage of 5-Star Reviews (Paid)
+![image_name](https://github.com/Mugunthan24/Amazon_Vine_Analysis/blob/main/Images/Percentage_of_Paid_5-Star_Reviews.png)
+
+The percentage of paid 5-star reviews is 48.9%.
+
+### Percentage of 5-Star Reviews (Unpaid)
+![image_name](https://github.com/Mugunthan24/Amazon_Vine_Analysis/blob/main/Images/Percentage_of_Unpaid_5-Star_Reviews.png)
+
+The percentage of unpaid 5-star reviews is 38.9%.
+
+## Summary
+The subsequent paragraphs will look at the potential presence of positivity bias and provide a recommendation for additional analysis.
+
+### Potential of Positivity Bias
+Looking at the percentage of paid 5-star reviews implies that nearly half of people who purchase the videogame will give it 5-stars, while the percentage of unpaid 5-star reviews implies that only 38.9% of those who purchase the video game will give it 5-stars. Positivity bias exists in the reviews for the Vine Program because the number of total reviews from the vine program (90), is substantially lower than the total reviews provided by those not in the vine program (37,831). Since the sample size for the non-vine reviews is higher, it is also more likely to be accurate.
+
+### Recommendation
+Instead of seperating the reviews by whether a review came from a vine-member or not, the reviews can be examined together. For example, getting the percentage of 5-star using the entire dataset and not seperating the reviews into groups. 
+
---
+++ Vine_Review_Analysis.sql
@@ -0,0 +1,85 @@
+CREATE TABLE review_id_table (
+  review_id TEXT PRIMARY KEY NOT NULL,
+  customer_id INTEGER,
+  product_id TEXT,
+  product_parent INTEGER,
+  review_date DATE -- this should be in the formate yyyy-mm-dd
+);
+
+-- This table will contain only unique values
+CREATE TABLE products_table (
+  product_id TEXT PRIMARY KEY NOT NULL UNIQUE,
+  product_title TEXT
+);
+
+-- Customer table for first data set
+CREATE TABLE customers_table (
+  customer_id INT PRIMARY KEY NOT NULL UNIQUE,
+  customer_count INT
+);
+
+-- vine table
+CREATE TABLE vine_table (
+  review_id TEXT PRIMARY KEY,
+  star_rating INTEGER,
+  helpful_votes INTEGER,
+  total_votes INTEGER,
+  vine TEXT,
+  verified_purchase TEXT
+);
+
+
+-- Vine Table filtered for total_votes greater than 20
+CREATE TABLE vine_table_filtered1
+	AS (SELECT * FROM vine_table
+			WHERE total_votes > 20)
+
+
+-- Vine Table filtered for total_votes greater than 20 and helpful_votes/total_votes is greater than 50%
+CREATE TABLE vine_table_filtered2
+	AS (SELECT * FROM vine_table_filtered1
+			WHERE CAST(helpful_votes AS FLOAT)/CAST(total_votes AS FLOAT) >=0.5)
+
+
+-- Vine Table filtered for total_votes greater than 20, helpful_votes/total_votes is greater than 50%, and a review was written as part of the Vine program (paid) 
+CREATE TABLE vine_table_filtered3
+	AS (SELECT * FROM vine_table_filtered2
+			WHERE vine = 'Y')
+
+
+-- Vine Table filtered for total_votes greater than 20, helpful_votes/total_votes is greater than 50%, and a review was not written as part of the Vine program (paid) 
+CREATE TABLE vine_table_filtered4
+	AS (SELECT * FROM vine_table_filtered2
+			WHERE vine = 'N')
+
+
+-- Total Number of Reviews (Paid)
+CREATE TABLE vine_table_number_of_reviews_paid
+    AS (SELECT COUNT(vine) AS "Total Number of Paid Reviews" FROM vine_table_filtered2
+	   		WHERE vine = 'Y')
+
+
+-- Total Number of Reviews (Unpaid)
+CREATE TABLE vine_table_number_of_reviews_unpaid
+    AS (SELECT COUNT(vine) AS "Total Number of Paid Reviews" FROM vine_table_filtered2
+	   		WHERE vine = 'N')
+
+
+-- Number of 5-star Reviews (Paid)
+CREATE TABLE vine_table_number_of_5Stars_paid
+    AS (SELECT COUNT(vine) AS "Total Number of 5-Star Paid Reviews" FROM vine_table_filtered2
+	   		WHERE star_rating = 5 AND vine = 'Y')
+
+
+-- Number of 5-star Reviews (Unpaid)
+CREATE TABLE vine_table_number_of_5Stars_unpaid
+    AS (SELECT COUNT(vine) AS "Total Number of 5-Star Unpaid Reviews" FROM vine_table_filtered2
+	   		WHERE star_rating = 5 AND vine = 'N')
+
+
+-- Percentage of 5-star Reviews (Paid)
+SELECT 
+((SELECT COUNT(verified_purchase) AS "Total Number of 5-Star Paid Reviews" FROM vine_table_filtered2
+	   		WHERE star_rating = 5 AND verified_purchase = 'Y') /
+(SELECT COUNT(verified_purchase) AS "Total Number of Paid Reviews" FROM vine_table_filtered2
+	   		WHERE verified_purchase = 'Y')) AS "Percentage of 5 Star Reviews (Paid)"
